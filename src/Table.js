bbc4de5e77fb862dbc241a93c645af85c04e38d5
Mudasir Zahoor <mudasir.zahoor@trialx.com>
2019-07-07 16:14:07 +0530

    Dynamic rows and columns displayed in table

---
+++ src/Table.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import App from './App';
+
+export default class Table extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            rows:[],
+            columns:[],
+            error:null,
+            is_loading:true
+        }
+    }
+    componentWillMount(){
+        const API_URL='http://165.227.117.36:8047/query.json'
+        const RAW_BODY={
+                "queryType": "SQL",
+                "query": "SELECT *  FROM dfs.`/root/30YRInvestigatorsData.parquet` order by NCTID limit 10 offset 30"
+            }
+        fetch(API_URL, {
+            method: "POST",
+            body: JSON.stringify(RAW_BODY),
+            headers: {
+              "Content-Type": "application/json",
+              "User-Name": "mapr"
+            }
+          }).then((response)=> {
+            if (response.status !== 200) {
+                console.log('Something Went Wrong' +
+                  response.status);
+                  alert('Something Went Wrong', response.status)
+                return;
+              }
+            response.json().then((data) => {
+                this.setState({
+                    is_loading:false,
+                    rows: data.rows,
+                    columns:data.columns
+                })
+                console.log(data);
+              });
+          }, (error)=> {
+            console.log('error')
+            alert("Error!!", error)
+          })
+    }
+    render(){
+        const {is_loading, rows, columns} = this.state
+        const normalized_rows = rows.map(function(obj) {
+            return Object.keys(obj).sort().map(function(key) {
+                return obj[key];
+            });
+          });
+        const normalized_columns = columns.sort()
+        console.log(normalized_columns, normalized_rows,'dd')
+        return (
+            is_loading ?<span>Loading...</span>: <App rows={normalized_rows} columns={normalized_columns}/>
+        )
+    }
+}
