f819d09c20da117d459e051c522e7f29eece5d28
mudasirzahoor <mudasir.zahoor@trialx.com>
2019-07-08 22:38:56 +0530

    backend pagination complete, useless files removed

--- src/App.js
+++ src/App.js
@@ -1,78 +1,113 @@
 import React from "react";
 import MUIDataTable from "mui-datatables";
+import { CircularProgress, Typography } from '@material-ui/core';
 
+const API_URL='http://165.227.117.36:8047/query.json'
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      rows:[],
+      columns:[],
+      error:null,
+      is_loading:true,
+      count:10000
+    }
+  }
+  componentWillMount(){
+    this.changePage()
+  }
+  changePage = (page=0,row_per_page=10) => {
+    this.setState({is_loading:true})
+    console.log(page,row_per_page,page*row_per_page)
+    let new_query=`SELECT *  FROM dfs.\`/root/30YRInvestigatorsData.parquet\` order by NCTID limit ${row_per_page} offset ${page*row_per_page}`
+    const RAW_BODY={
+      "queryType": "SQL",
+      "query": new_query
+    }
+    fetch(API_URL, {
+      method: "POST",
+      body: JSON.stringify(RAW_BODY),
+      headers: {
+        "Content-Type": "application/json",
+        "User-Name": "mapr"
+      }
+    }).then((response)=> {
+      if (response.status !== 200) {
+          console.log('Something Went Wrong' +
+            response.status);
+            alert('Something Went Wrong', response.status)
+            this.setState({is_loading:false,rows:[{}],columns:[]})
+          return;
+        }
+      response.json().then((data) => {
+        let rows =data.rows
+        let columns=data.columns
+        const normalized_rows = rows.map(function(obj) {
+          return Object.keys(obj).sort().map(function(key) {
+              return obj[key];
+          });
+        });
+      const normalized_columns = columns.sort()
+          this.setState({
+              is_loading:false,
+              rows: normalized_rows,
+              columns:normalized_columns
+          })
+          console.log(data);
+        });
+    }, (error)=> {
+      console.log('error')
+      alert("Error!!", error)
+      this.setState({is_loading:false,rows:[{}],columns:[]})
+    })
+  };
+
+
   render() {
-    // const columns = ["Name", "Title", "Location", "Age", "Salary"];
-
-    // const data = [
-    //   ["Gabby George", "Business Analyst", "Minneapolis", 30, "$100,000"],
-    //   ["Aiden Lloyd", "Business Consultant", "Dallas", 55, "$200,000"],
-    //   ["Jaden Collins", "Attorney", "Santa Ana", 27, "$500,000"],
-    //   ["Franky Rees", "Business Analyst", "St. Petersburg", 22, "$50,000"],
-    //   ["Aaren Rose", "Business Consultant", "Toledo", 28, "$75,000"],
-    //   [
-    //     "Blake Duncan",
-    //     "Business Management Analyst",
-    //     "San Diego",
-    //     65,
-    //     "$94,000"
-    //   ],
-    //   ["Frankie Parry", "Agency Legal Counsel", "Jacksonville", 71, "$210,000"],
-    //   ["Lane Wilson", "Commercial Specialist", "Omaha", 19, "$65,000"],
-    //   ["Robin Duncan", "Business Analyst", "Los Angeles", 20, "$77,000"],
-    //   ["Mel Brooks", "Business Consultant", "Oklahoma City", 37, "$135,000"],
-    //   ["Harper White", "Attorney", "Pittsburgh", 52, "$420,000"],
-    //   ["Kris Humphrey", "Agency Legal Counsel", "Laredo", 30, "$150,000"],
-    //   ["Frankie Long", "Industrial Analyst", "Austin", 31, "$170,000"],
-    //   ["Brynn Robbins", "Business Analyst", "Norfolk", 22, "$90,000"],
-    //   ["Justice Mann", "Business Consultant", "Chicago", 24, "$133,000"],
-    //   [
-    //     "Addison Navarro",
-    //     "Business Management Analyst",
-    //     "New York",
-    //     50,
-    //     "$295,000"
-    //   ],
-    //   ["Jesse Welch", "Agency Legal Counsel", "Seattle", 28, "$200,000"],
-    //   ["Eli Mejia", "Commercial Specialist", "Long Beach", 65, "$400,000"],
-    //   ["Gene Leblanc", "Industrial Analyst", "Hartford", 34, "$110,000"],
-    //   ["Danny Leon", "Computer Scientist", "Newark", 60, "$220,000"],
-    //   ["Lane Lee", "Corporate Counselor", "Cincinnati", 52, "$180,000"],
-    //   ["Jesse Hall", "Business Analyst", "Baltimore", 44, "$99,000"],
-    //   ["Danni Hudson", "Agency Legal Counsel", "Tampa", 37, "$90,000"],
-    //   ["Terry Macdonald", "Commercial Specialist", "Miami", 39, "$140,000"],
-    //   ["Justice Mccarthy", "Attorney", "Tucson", 26, "$330,000"],
-    //   ["Silver Carey", "Computer Scientist", "Memphis", 47, "$250,000"],
-    //   ["Franky Miles", "Industrial Analyst", "Buffalo", 49, "$190,000"],
-    //   ["Glen Nixon", "Corporate Counselor", "Arlington", 44, "$80,000"],
-    //   [
-    //     "Gabby Strickland",
-    //     "Business Process Consultant",
-    //     "Scottsdale",
-    //     26,
-    //     "$45,000"
-    //   ],
-    //   ["Mason Ray", "Computer Scientist", "San Francisco", 39, "$142,000"]
-    // ];
-
     const options = {
       filterType: "dropdown",
-      responsive: "scroll",
+      // responsive: "scroll",
+      responsive: 'scroll',
+      serverSide: true,
+      count: this.state.count,
+      page: this.state.page,
       textLabels: {
         body: {
           noMatch: "Sorry, no records found for the given query..",
         }
+      },
+      onTableChange: (action, tableState) => {
+        // console.log(action, tableState);
+        switch (action) {
+          case 'changePage':
+            this.changePage(tableState.page,tableState.rowsPerPage);
+            break;
+          case 'changeRowsPerPage':
+            this.changePage(tableState.page,tableState.rowsPerPage);
+            break;
+        }
       }
     };
-
     return (
       <MUIDataTable
-        title={"Dynamic Table"}
-        data={this.props.rows}
-        columns={this.props.columns}
+        // title={"Dynamic Table"}
+        data={this.state.rows}
+        columns={this.state.columns}
         options={options}
+        title={<Typography variant="title">
+          Dynamic Table
+          {this.state.is_loading && <CircularProgress size={24} style={{marginLeft: 15, position: 'relative', top: 4}} />}
+          </Typography>}
       />
     );
   }
 }
+// function Loader(){
+//   return(
+//     <div class="loader">
+//       <div class="ball"></div>
+//       <span>Fetching Data...</span>
+//     </div>
+//   )
+// }
